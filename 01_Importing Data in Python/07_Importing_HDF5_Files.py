c30b32ddcc21b072ecbd7be96f3846475858f179
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-11-19 14:15:24 +0530

    Importing HDF5 Files

---
+++ 01_Importing Data in Python/07_Importing_HDF5_Files.py
@@ -0,0 +1,48 @@
+# HDF5
+# Hierarchical Data Format version 5
+# Standard for storing large quantities of numerical data.
+# Datasets can be hunderds of gigabytes or terabytes.
+# HDF5 can scale to exabytes
+
+# Using h5py to import HDF5 files
+# The file 'LIGO_data.hdf5' is already in your working directory. In this exercise, you'll import it using the h5py library. You'll also print out its datatype to confirm you have imported it correctly. You'll then study the structure of the file in order to see precisely what HDF groups it contains.
+
+# Import packages
+import numpy as np
+import h5py
+
+# Assign filename: file
+file = '../Dataset/Importing Data From Dataset/LIGO_data.hdf5'
+
+# Load file: data
+data = h5py.File(file, 'r')
+
+# Print the datatype of the loaded file
+print(type(data))
+
+# Print the keys of the file
+for key in data.keys():
+    print(key)
+
+# Extracting data from your HDF5 file
+# Get the HDF5 group: group
+group = data['strain']
+
+# Check out keys of group
+for key in group.keys():
+    print(key)
+
+# Set variable equal to time series data: strain
+strain = data['strain']['Strain'].value
+
+# Set number of time points to sample: num_samples
+num_samples = 10000
+
+# Set time vector
+time = np.arange(0, 1, 1/num_samples)
+
+# Plot data
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
