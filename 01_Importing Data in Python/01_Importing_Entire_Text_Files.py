00aec5cc7340907cdf25cfc92f15f199d17c36e8
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-11-19 09:25:14 +0530

    Importing Entire Text File

---
+++ 01_Importing Data in Python/01_Importing_Entire_Text_Files.py
@@ -0,0 +1,14 @@
+# Open a file: file
+file = open('../Dataset/Importing Data From Dataset/moby_dick.txt', mode = 'r')
+
+# Print it
+print(file.read())
+
+# Check whether file is closed
+print(file.closed)
+
+# Close file
+file.close()
+
+# Check whether file is closed
+print(file.closed)
