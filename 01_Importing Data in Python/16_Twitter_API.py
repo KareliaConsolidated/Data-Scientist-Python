d2eedde224b65179f31ece9385e92ef962130de5
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-11-20 12:42:12 +0530

    Twitter API Request

---
+++ 01_Importing Data in Python/16_Twitter_API.py
@@ -0,0 +1,44 @@
+# Import package
+import tweepy
+
+# Store OAuth authentication credentials in relevant variables
+access_token = "1092294848-aHN7DcRP9B4VMTQIhwqOYiB14YkW92fFO8k8EPy"
+access_token_secret = "X4dHmhPfaksHcQ7SCbmZa2oYBBVSD2g8uIHXsp5CTaksx"
+consumer_key = "nZ6EA0FxZ293SxGNg8g8aP0HM"
+consumer_secret = "fJGEodwe3KiKUnsYJC3VRndj7jevVvXbK2D5EiJ2nehafRgA6i"
+
+# Pass OAuth details to tweepy's OAuth handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+#=========Tweet Stream Listener Class===========#
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api=None):
+        super(MyStreamListener, self).__init__()
+        self.num_tweets = 0
+        self.file = open("../Dataset/Importing Data From Dataset/tweets.txt", "w")
+
+    def on_status(self, status):
+        tweet = status._json
+        self.file.write( json.dumps(tweet) + '\n' )
+        self.num_tweets += 1
+        if self.num_tweets < 100:
+            return True
+        else:
+            return False
+        self.file.close()
+
+    def on_error(self, status):
+        print(status)
+#============================================#
+
+# Initialize Stream listener
+l = MyStreamListener()
+
+# Create your Stream object with authentication
+stream = tweepy.Stream(auth, l)
+
+# Filter Twitter Streams to capture data by the keywords:
+stream.filter(['clinton', 'trump', 'sanders', 'cruz'])
+
+#==========Load and explore your Twitter data============#
