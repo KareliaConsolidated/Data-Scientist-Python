81d55589a4a88228b4ef6f8009fb0a2b4ea947f2
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-05-10 18:19:11 +0530

    Time Zones and Daylight Saving

--- 28_Working_with_Dates_and_Times_in_Python/03_Time_Zones_and_Daylight_Saving.py
+++ 28_Working_with_Dates_and_Times_in_Python/03_Time_Zones_and_Daylight_Saving.py
@@ -165,3 +165,116 @@ print(notlocal.isoformat())
 # You already have a datetime called start, set for March 12, 2017 at midnight, set to the timezone 'America/New_York'.
 
 # Add six hours to start and assign it to end. Look at the UTC offset for the two results.
+# Import datetime, timedelta, tz, timezone
+from datetime import datetime, timedelta, timezone
+from dateutil import tz
+
+# Start on March 12, 2017, midnight, then add 6 hours
+start = datetime(2017, 3, 12, tzinfo = tz.gettz('America/New_York'))
+end = start + timedelta(hours=6)
+print(start.isoformat() + " to " + end.isoformat())
+
+############# Instructions ##############  
+# You added 6 hours, and got 6 AM, despite the fact that the clocks springing forward means only 5 hours would have actually elapsed!
+
+# Calculate the time between start and end. How much time does Python think has elapsed?
+# Import datetime, timedelta, tz, timezone
+from datetime import datetime, timedelta, timezone
+from dateutil import tz
+
+# Start on March 12, 2017, midnight, then add 6 hours
+start = datetime(2017, 3, 12, tzinfo = tz.gettz('America/New_York'))
+end = start + timedelta(hours=6)
+print(start.isoformat() + " to " + end.isoformat())
+
+# How many hours have elapsed?
+print((end - start).total_seconds()/(60*60))
+
+############# Instructions ##############  
+# Import datetime, timedelta, tz, timezone
+from datetime import datetime, timedelta, timezone
+from dateutil import tz
+
+# Start on March 12, 2017, midnight, then add 6 hours
+start = datetime(2017, 3, 12, tzinfo = tz.gettz('America/New_York'))
+end = start + timedelta(hours=6)
+print(start.isoformat() + " to " + end.isoformat())
+
+# How many hours have elapsed?
+print((end - start).total_seconds()/(60*60))
+
+# What if we move to UTC?
+print((end.astimezone(timezone.utc) - start.astimezone(timezone.utc))\
+      .total_seconds()/(60*60))
+
+############### Exercise ################
+# March 29, throughout a decade
+# Daylight Saving rules are complicated: they're different in different places, they change over time, and they usually start on a Sunday (and so they move around the calendar).
+
+# For example, in the United Kingdom, as of the time this lesson was written, Daylight Saving begins on the last Sunday in March. Let's look at the UTC offset for March 29, at midnight, for the years 2000 to 2010.
+
+############# Instructions ############## 
+# Using tz, set the timezone for dt to be 'Europe/London'.
+# Within the for loop:
+# Use the .replace() method to change the year for dt to be y.
+# Call .isoformat() on the result to observe the results.
+
+# Import datetime and tz
+from datetime import datetime
+from dateutil import tz
+
+# Create starting date
+dt = datetime(2000, 3, 29, tzinfo = tz.gettz('Europe/London'))
+
+# Loop over the dates, replacing the year, and print the ISO timestamp
+for y in range(2000, 2011):
+  print(dt.replace(year=y).isoformat())
+
+############### Exercise ################
+# Finding ambiguous datetimes
+# At the end of lesson 2, we saw something anomalous in our bike trip duration data. Let's see if we can identify what the problem might be.
+
+# The data has is loaded as onebike_datetimes, and tz has already been imported from dateutil.
+
+############# Instructions ##############   
+# Loop over the trips in onebike_datetimes:
+# Print any rides whose start is ambiguous.
+# Print any rides whose end is ambiguous.
+
+# Loop over trips
+for trip in onebike_datetimes:
+  # Rides with ambiguous start
+  if tz.datetime_ambiguous(trip['start']):
+    print("Ambiguous start at " + str(trip['start']))
+  # Rides with ambiguous end
+  if tz.datetime_ambiguous(trip['end']):
+    print("Ambiguous end at " + str(trip['end']))
+
+############### Exercise ################
+# Cleaning daylight saving data with fold
+# As we've just discovered, there is a ride in our data set which is being messed up by a Daylight Savings shift. Let's clean up the data set so we actually have a correct minimum ride length. We can use the fact that we know the end of the ride happened after the beginning to fix up the duration messed up by the shift out of Daylight Savings.
+
+# Since Python does not handle tz.enfold() when doing arithmetic, we must put our datetime objects into UTC, where ambiguities have been resolved.
+
+# onebike_datetimes is already loaded and in the right timezone. tz and timezone have been imported. Use tz.UTC for your timezone.
+
+############# Instructions ##############       
+# Complete the if statement to be true only when a ride's start comes after its end.
+# When start is after end, call tz.enfold() on the end so you know it refers to the one after the daylight savings time change.
+# After the if statement, convert the start and end to UTC so you can make a proper comparison.
+
+trip_durations = []
+for trip in onebike_datetimes:
+  # When the start is later than the end, set the fold to be 1
+  if trip['start'] > trip['end']:
+    trip['end'] = tz.enfold(trip['end'])
+  # Convert to UTC
+  start = trip['start'].astimezone(tz.UTC)
+  end = trip['end'].astimezone(tz.UTC)
+
+  # Subtract the difference
+  trip_length_seconds = (end-start).total_seconds()
+  trip_durations.append(trip_length_seconds)
+
+# Take the shortest trip duration
+print("Shortest trip: " + str(min(trip_durations)))
